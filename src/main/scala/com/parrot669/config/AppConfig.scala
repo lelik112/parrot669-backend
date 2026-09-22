862308340a913018398baf2ec74f78390c93a7b7
lelik112 <30652773+lelik112@users.noreply.github.com>
2026-09-22 14:57:54 +0300

    Keep transactional sender address provider-neutral

--- src/main/scala/com/parrot669/config/AppConfig.scala
+++ src/main/scala/com/parrot669/config/AppConfig.scala
@@ -61,9 +61,7 @@ object AppConfig {
         }
 
       val emailFrom =
-        nonEmpty(env, "PARROT_EMAIL_FROM").getOrElse(
-          "PARROT 669 <hello@parrot669.com>"
-        )
+        nonEmpty(env, "PARROT_EMAIL_FROM").getOrElse("hello@parrot669.com")
 
       val resendEmailConfig =
         nonEmpty(env, "RESEND_API_KEY").map { apiKey =>
