34be42fffc8b62131511b5125d317580144dcd3a
lelik112 <30652773+lelik112@users.noreply.github.com>
2026-09-22 14:43:30 +0300

    Wire verification email sender into auth

--- src/main/scala/com/parrot669/Main.scala
+++ src/main/scala/com/parrot669/Main.scala
@@ -6,7 +6,12 @@ import com.comcast.ip4s.{Host, Port}
 import com.parrot669.config.AppConfig
 import com.parrot669.db.Database
 import com.parrot669.http.Routes
-import com.parrot669.integration.HttpIcalFetcher
+import com.parrot669.integration.{
+  CloudflareVerificationEmailSender,
+  HttpIcalFetcher,
+  LoggingVerificationEmailSender,
+  VerificationEmailSender
+}
 import com.parrot669.repo.{AuthRepository, ParrotRepository}
 import com.parrot669.service.{AuthService, ParrotService}
 import org.http4s.ember.server.EmberServerBuilder
@@ -36,8 +41,23 @@ object Main extends IOApp.Simple {
         repo = new ParrotRepository[IO](xa)
         authRepo = new AuthRepository[IO](xa)
         icalFetcher = new HttpIcalFetcher[IO](allowLocalhost = config.environment == "test")
+        emailSender: VerificationEmailSender[IO] =
+          config.email match {
+            case Some(email) =>
+              new CloudflareVerificationEmailSender[IO](
+                email.cloudflareAccountId,
+                email.cloudflareApiToken,
+                email.from
+              )
+            case None =>
+              new LoggingVerificationEmailSender[IO]
+          }
         service = new ParrotService[IO](repo, icalFetcher)
-        authService = new AuthService[IO](authRepo)
+        authService = new AuthService[IO](
+          authRepo,
+          emailSender,
+          config.publicBaseUrl
+        )
         routes = new Routes[IO](
           service,
           authService,
