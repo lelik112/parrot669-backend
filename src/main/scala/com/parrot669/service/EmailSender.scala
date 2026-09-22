ee69cdc76057416bb433f5a02139d1003bfc6873
lelik112 <30652773+lelik112@users.noreply.github.com>
2026-09-22 16:03:44 +0300

    Add unconfigured email sender

--- src/main/scala/com/parrot669/service/EmailSender.scala
+++ src/main/scala/com/parrot669/service/EmailSender.scala
@@ -11,4 +11,11 @@ object EmailSender {
     override def sendVerificationEmail(email: String, token: String): F[Unit] =
       Async[F].unit
   }
+
+  def unconfigured[F[_]: Async]: EmailSender[F] = new EmailSender[F] {
+    override def sendVerificationEmail(email: String, token: String): F[Unit] =
+      Async[F].raiseError(
+        new IllegalStateException("email delivery is not configured")
+      )
+  }
 }
